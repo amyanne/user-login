91a4b58106ad2e8f4ac024cc1258efe10ea03ff3
Amy Westcott <amy.westcott@yahoo.com>
2020-08-25 16:56:38 -0600

    user login works

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-//var createError = require('http-errors');
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var session = require("express-session");
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const dashboardRouter = require("./routes/dashboard");         
+const publicRouter = require("./routes/public");
+const usersRouter = require("./routes/users");
 
 var app = express();
+var oktaClient = new okta.Client({
+  orgUrl: 'https://dev-959947-admin.okta.com',
+  token: '00b0YbjW0bKn6PNyB8BOi1okVmqkgTVr3yRNnLBd8e'
+});
+const oidc = new ExpressOIDC({
+  issuer: "https://dev-959947-admin.okta.com/oauth2/default",
+  client_id: "0oarwrfbpg0Ek0c7P4x6",
+  client_secret: "F5vVW9WE_oOshC7ophC7Ra5ZEoV7L48AHTtiBG5B",
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,24 +45,48 @@ app.use(session({
   resave: true,
   saveUninitialized: false
 }));
+app.use(oidc.router);
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
 
-app.use('/', indexRouter);
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
+
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+}
+
+app.use('/', publicRouter);
+app.use('/dashboard', loginRequired, dashboardRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-  //next(createError(404));
-//});
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-//app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
- // res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  //res.status(err.status || 500);
-  //res.render('error');
-//});
+  //render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
